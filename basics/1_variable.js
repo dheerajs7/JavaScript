97f243b698cd456488a3b3cdd0921475fe97bf63
dheerajs7 <dheeraj.salvi293@gmail.com>
2024-09-14 17:25:34 +0530

    datatypes and conversation

---
+++ basics/1_variable.js
@@ -0,0 +1,14 @@
+const accountId = 8011
+let accountEmail ="dheeraj@gmail.com"
+var accountPassword = "dheeraj123"
+accountCity = "Udaipur"
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/*
+Prefer not to use var 
+because of issue of block scope and functional scope
+
+Always use let or const
+*/
---
+++ basics/2_datatypes.js
@@ -0,0 +1,23 @@
+let age = 19 //number
+let name = "John" //string
+let loggedIn= false //boolean
+let state //undefined
+let power =null //null its type is object
+//bigint
+//symbol
+
+//object
+
+//  
+
+const score =true
+
+
+
+
+console.log(typeof Number(score));
+console.log(Number(score));
+
+//"45" => 45(number)
+//"45abc" => NaN(but is type is number)
+//true =>1; false=>0
